f8c4bfe9c56c2799652f18dbe19a86e174812559
Cong Thanh <vcthanh20@gmail.com>
2024-05-30 23:10:55 +0700

    init

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+
+
+class Recipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
+
+    def layout(self):
+        self.folders.generators = "conan"
+
+    def requirements(self):
+        self.requires("spdlog/1.14.1")
+        self.requires("sqlitecpp/3.3.1")
+        self.requires("asio/1.30.2")
+
+    def build_requirements(self):
+        self.test_requires("gtest/1.14.0")
---
+++ source/main.cpp
@@ -0,0 +1,7 @@
+#include <spdlog/spdlog.h>
+
+auto main() -> int
+{
+  spdlog::info("Hello world");
+  return 0;
+}
